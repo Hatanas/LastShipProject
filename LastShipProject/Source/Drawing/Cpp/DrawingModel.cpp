3fc8d3bbccdb91dc9eeaa7b3f2ee52dc8837c44d
Junpaku <itzimeru@gmail.com>
2014-12-23 05:37:44 +0900

    SceneManager完成

--- LastShipProject/Source/Drawing/Cpp/DrawingModel.cpp
+++ LastShipProject/Source/Drawing/Cpp/DrawingModel.cpp
@@ -1,7 +1,30 @@
-#include "./../DrawingModel.h"
+#include "../DrawingModel.h"
 
+inline DrawingModel::DrawingModel(){}
 
-DrawingModel::DrawingModel(){}
+inline DrawingModel::DrawingModel(BaseModel *model)
+{
+	setModel(model->getHandle()); 
+}
 
+inline DrawingModel::DrawingModel(int handle)
+{
+	setModel(handle);
+}
 
-DrawingModel::~DrawingModel(){}
+inline void DrawingModel::setModel(int handle)
+{
+	modelHandle_m = handle; 
+}
+
+inline void DrawingModel::setModel(BaseModel *model)
+{
+	setModel(model->getHandle());
+}
+
+inline void DrawingModel::draw()
+{
+	MV1DrawModel(modelHandle_m);
+}
+
+inline DrawingModel::~DrawingModel(){}
