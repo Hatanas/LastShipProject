521683eaf025df2fc71798089c20712bd256f92a
Junpaku <itzimeru@gmail.com>
2015-01-16 15:10:28 +0900

    LastShipGameクラス作った

---
+++ LastShipProject/Source/Game/BaseDxGame.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cassert>
+#include "DxLib.h"
+#include "GameInterface.h"
+
+class BaseDxGame : public GameInterface
+{
+private:
+	bool flgHasFinished = false;
+protected:
+	virtual int initialize() = 0;
+	virtual int finalize() = 0;
+	virtual int update() = 0;
+	virtual bool continues() = 0;
+	void finish();
+	bool hasFinished();
+public:
+	int play();
+	BaseDxGame() {}
+	virtual ~BaseDxGame() {}
+};
+
